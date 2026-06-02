1ec799bfad36439676ba1a226d1b016b09bbb021
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-06-02 14:17:50 +0300

    Update package.js

--- apps/game/package.js
+++ apps/game/package.js
@@ -1,1 +1,15 @@
+{
+  "name": "aerostar-fiesta-game",
+  "version": "1.0.0",
+  "description": "Виртуальные соревнования аэростатов для проекта aerost.art",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "nedb-promises": "^6.2.1",
+    "socket.io": "^4.7.5"
+  }
+}
 
