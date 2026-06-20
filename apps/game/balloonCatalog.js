51b1fd4bd37a7308cdb44142ed2a0cdf36956a52
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-06-20 15:46:50 +0300

    Update image paths in balloon catalog

--- apps/game/balloonCatalog.js
+++ apps/game/balloonCatalog.js
@@ -3,16 +3,16 @@
  * Используются изображения из /apps/frontend/images/
  */
 const BALLOON_STYLES = {
-    'bal1': { id: 'bal1', name: 'Шар №1', image: '/frontend/images/bal1.png' },
-    'bal2': { id: 'bal2', name: 'Шар №2', image: '/frontend/images/bal2.png' },
-    'bal3': { id: 'bal3', name: 'Шар №3', image: '/frontend/images/bal3.png' },
-    'bal4': { id: 'bal4', name: 'Шар №4', image: '/frontend/images/bal4.png' },
-    'bal5': { id: 'bal5', name: 'Шар №5', image: '/frontend/images/bal5.png' },
-    'bal6': { id: 'bal6', name: 'Шар №6', image: '/frontend/images/bal6.png' },
-    'bal7': { id: 'bal7', name: 'Шар №7', image: '/frontend/images/bal7.png' },
-    'bal8': { id: 'bal8', name: 'Шар №8', image: '/frontend/images/bal8.png' },
-    'bal9': { id: 'bal9', name: 'Шар №9', image: '/frontend/images/bal9.png' },
-    'bal10': { id: 'bal10', name: 'Шар №10', image: '/frontend/images/bal10.png' }
+    'bal1': { id: 'bal1', name: 'Шар №1', image: '/images/bal1.png' },
+    'bal2': { id: 'bal2', name: 'Шар №2', image: '/images/bal2.png' },
+    'bal3': { id: 'bal3', name: 'Шар №3', image: '/images/bal3.png' },
+    'bal4': { id: 'bal4', name: 'Шар №4', image: '/images/bal4.png' },
+    'bal5': { id: 'bal5', name: 'Шар №5', image: '/images/bal5.png' },
+    'bal6': { id: 'bal6', name: 'Шар №6', image: '/images/bal6.png' },
+    'bal7': { id: 'bal7', name: 'Шар №7', image: '/images/bal7.png' },
+    'bal8': { id: 'bal8', name: 'Шар №8', image: '/images/bal8.png' },
+    'bal9': { id: 'bal9', name: 'Шар №9', image: '/images/bal9.png' },
+    'bal10': { id: 'bal10', name: 'Шар №10', image: '/images/bal10.png' }
 };
 
 module.exports = BALLOON_STYLES;
