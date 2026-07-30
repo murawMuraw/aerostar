c5839e57a9b4f23921a8f2c7fca61521c50aba9b
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-07-30 19:36:35 +0300

    Add Ship class with initial properties

---
+++ apps/game_1/models/Ship.js
@@ -0,0 +1,25 @@
+class Ship{
+
+    constructor(type){
+
+        this.type=type;
+
+        this.owner=null;
+
+        this.lat=0;
+
+        this.lng=0;
+
+        this.heading=0;
+
+        this.speed=0;
+
+        this.rudder=0;
+
+        this.sail=100;
+
+        this.anchor=true;
+
+    }
+
+}
