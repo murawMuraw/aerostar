b3ae62ad39075b088eb2c3c0336cd74ca23efe40
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-07-30 19:38:57 +0300

    Create PlayerManager class for player management

---
+++ apps/game_1/ managers/PlayerManager.js
@@ -0,0 +1,29 @@
+class PlayerManager{
+
+    constructor(){
+
+        this.players=new Map();
+
+    }
+
+    createPlayer(){
+
+    }
+
+    getPlayer(){
+
+    }
+
+    removePlayer(){
+
+    }
+
+    reconnect(){
+
+    }
+
+    disconnect(){
+
+    }
+
+}
