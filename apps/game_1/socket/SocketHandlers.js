d91c8e3f322be1673093845f34d36968775a50dc
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-07-30 19:44:05 +0300

    Add SocketHandlers for game socket events

---
+++ apps/game_1/socket/SocketHandlers.js
@@ -0,0 +1,31 @@
+module.exports=function(io,game){
+
+    io.on("connection",(socket)=>{
+
+        socket.on("join", (...a)=>{
+
+        });
+
+        socket.on("reconnect", (...a)=>{
+
+        });
+
+        socket.on("turn", (...a)=>{
+
+        });
+
+        socket.on("sail", (...a)=>{
+
+        });
+
+        socket.on("anchor", (...a)=>{
+
+        });
+
+        socket.on("disconnect", ()=>{
+
+        });
+
+    });
+
+};
