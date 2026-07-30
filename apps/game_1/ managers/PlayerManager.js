c85117c72dafbeff68ce477725ce201083c12fb3
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-07-30 19:57:47 +0300

    Refactor PlayerManager class and methods

--- apps/game_1/ managers/PlayerManager.js
+++ apps/game_1/ managers/PlayerManager.js
@@ -1,29 +1,154 @@
-class PlayerManager{
+// managers/PlayerManager.js
 
-    constructor(){
+const Player = require("../models/Player");
 
-        this.players=new Map();
+class PlayerManager {
 
-    }
+    constructor() {
 
-    createPlayer(){
-
-    }
-
-    getPlayer(){
+        // userId -> Player
+        this.players = new Map();
 
     }
 
-    removePlayer(){
+    /**
+     * Создать игрока
+     */
+    create(userId) {
+
+        if (this.players.has(userId)) {
+            return this.players.get(userId);
+        }
+
+        const player = new Player(userId);
+
+        this.players.set(userId, player);
+
+        return player;
 
     }
 
-    reconnect(){
+    /**
+     * Получить игрока
+     */
+    get(userId) {
+
+        return this.players.get(userId) || null;
 
     }
 
-    disconnect(){
+    /**
+     * Проверить существование
+     */
+    exists(userId) {
+
+        return this.players.has(userId);
+
+    }
+
+    /**
+     * Удалить игрока
+     */
+    remove(userId) {
+
+        return this.players.delete(userId);
+
+    }
+
+    /**
+     * Подключение сокета
+     */
+    connect(userId, socketId) {
+
+        const player = this.get(userId);
+
+        if (!player) return null;
+
+        player.connect(socketId);
+
+        return player;
+
+    }
+
+    /**
+     * Отключение сокета
+     */
+    disconnect(userId) {
+
+        const player = this.get(userId);
+
+        if (!player) return;
+
+        player.disconnect();
+
+    }
+
+    /**
+     * Назначить корабль
+     */
+    assignShip(userId, ship) {
+
+        const player = this.get(userId);
+
+        if (!player) return false;
+
+        player.assignShip(ship);
+
+        return true;
+
+    }
+
+    /**
+     * Удалить корабль
+     */
+    removeShip(userId) {
+
+        const player = this.get(userId);
+
+        if (!player) return false;
+
+        player.removeShip();
+
+        return true;
+
+    }
+
+    /**
+     * Все игроки
+     */
+    getAll() {
+
+        return Array.from(this.players.values());
+
+    }
+
+    /**
+     * Только подключённые
+     */
+    getOnline() {
+
+        return this.getAll().filter(player => player.connected);
+
+    }
+
+    /**
+     * Количество игроков
+     */
+    count() {
+
+        return this.players.size;
+
+    }
+
+    /**
+     * Очистить
+     */
+    clear() {
+
+        this.players.clear();
 
     }
 
 }
+
+module.exports = PlayerManager;
