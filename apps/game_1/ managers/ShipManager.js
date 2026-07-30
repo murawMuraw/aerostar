0822455ae05579b0b712d8558578a4d311a60276
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-07-30 19:39:36 +0300

    Add ShipManager class for managing ships

---
+++ apps/game_1/ managers/ShipManager.js
@@ -0,0 +1,25 @@
+class ShipManager{
+
+    constructor(){
+
+        this.ships=new Map();
+
+    }
+
+    createShip(){
+
+    }
+
+    assignShip(){
+
+    }
+
+    releaseShip(){
+
+    }
+
+    updateShip(){
+
+    }
+
+}
