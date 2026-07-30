eb97f6e41b61bde42f87c317cfdfd01dcd929898
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-07-30 19:35:55 +0300

    Add Player class for game_1 model

---
+++ apps/game_1/models/Player.js
@@ -0,0 +1,17 @@
+class Player{
+
+    constructor(userId){
+
+        this.userId=userId;
+
+        this.sessionId=null;
+
+        this.socketId=null;
+
+        this.ship=null;
+
+        this.connected=false;
+
+    }
+
+}
