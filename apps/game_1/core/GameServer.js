4bc86717140ca488bea8f4c925946aafe3dbcb28
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-07-30 19:34:42 +0300

    Create GameServer class for game management

---
+++ apps/game_1/core/GameServer.js
@@ -0,0 +1,24 @@
+class GameServer {
+
+    constructor(io){
+
+        this.io = io;
+
+        this.sessions = null;
+        this.players = null;
+        this.ships = null;
+        this.wind = null;
+        this.ocean = null;
+        this.race = null;
+
+    }
+
+    start(){
+
+    }
+
+    stop(){
+
+    }
+
+}
