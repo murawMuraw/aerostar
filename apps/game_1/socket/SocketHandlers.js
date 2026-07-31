8828f3516e9e5429b4f300ec5aff68047d0609bf
murawMuraw <53973384+murawMuraw@users.noreply.github.com>
2026-07-31 05:50:50 +0300

    Refactor socket handlers for improved structure

--- apps/game_1/socket/SocketHandlers.js
+++ apps/game_1/socket/SocketHandlers.js
@@ -1,28 +1,152 @@
-module.exports=function(io,game){
+// socket/SocketHandlers.js
 
-    io.on("connection",(socket)=>{
+module.exports = function registerSocketHandlers(io, game) {
 
-        socket.on("join", (...a)=>{
+    io.on("connection", (socket) => {
+
+        console.log(`Client connected: ${socket.id}`);
+
+        //------------------------------------------------------
+        // JOIN
+        //------------------------------------------------------
+
+        socket.on("join", ({ userId, shipType }) => {
+
+            try {
+
+                game.connect(userId, socket.id);
+
+                const ship = game.joinRace(userId, shipType);
+
+                if (!ship) {
+
+                    socket.emit("join_error", {
+                        message: "Ship unavailable"
+                    });
+
+                    return;
+
+                }
+
+                socket.emit("joined", {
+                    shipId: ship.id
+                });
+
+                game.broadcastState();
+
+            }
+            catch (err) {
+
+                console.error(err);
+
+            }
 
         });
 
-        socket.on("reconnect", (...a)=>{
+        //------------------------------------------------------
+        // RECONNECT
+        //------------------------------------------------------
+
+        socket.on("reconnect", ({ userId }) => {
+
+            try {
+
+                game.connect(userId, socket.id);
+
+                socket.emit("reconnected");
+
+                game.broadcastState();
+
+            }
+            catch (err) {
+
+                console.error(err);
+
+            }
 
         });
 
-        socket.on("turn", (...a)=>{
+        //------------------------------------------------------
+        // RUDDER
+        //------------------------------------------------------
+
+        socket.on("rudder", ({ userId, value }) => {
+
+            game.setRudder(userId, value);
 
         });
 
-        socket.on("sail", (...a)=>{
+        //------------------------------------------------------
+        // HEADING
+        //------------------------------------------------------
+
+        socket.on("heading", ({ userId, value }) => {
+
+            game.setHeading(userId, value);
 
         });
 
-        socket.on("anchor", (...a)=>{
+        //------------------------------------------------------
+        // SAIL
+        //------------------------------------------------------
+
+        socket.on("sail", ({ userId, value }) => {
+
+            game.setSail(userId, value);
 
         });
 
-        socket.on("disconnect", ()=>{
+        //------------------------------------------------------
+        // ANCHOR
+        //------------------------------------------------------
+
+        socket.on("anchor", ({ userId, value }) => {
+
+            game.setAnchor(userId, value);
+
+        });
+
+        //------------------------------------------------------
+        // POSITION
+        //------------------------------------------------------
+
+        socket.on("position", ({ userId, lat, lng }) => {
+
+            game.updatePosition(
+                userId,
+                lat,
+                lng
+            );
+
+        });
+
+        //------------------------------------------------------
+        // CHAT
+        //------------------------------------------------------
+
+        socket.on("chat", (msg) => {
+
+            io.emit("chat", msg);
+
+        });
+
+        //------------------------------------------------------
+        // DISCONNECT
+        //------------------------------------------------------
+
+        socket.on("disconnect", () => {
+
+            const player =
+                game.players.getBySocket(socket.id);
+
+            if (!player)
+                return;
+
+            game.disconnect(player.userId);
+
+            console.log(`Disconnected ${player.userId}`);
+
+            game.broadcastState();
 
         });
 
